1b90ecf5daa438b13fa9e14e815d7c2dc3227849
Maxim Antsiferov <90709563+MaximAntsiferov@users.noreply.github.com>
2022-08-12 18:46:33 +0700

    Configure db settings and create tables using sqlalchemy

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
 
+from source.db import database, engine, metadata
 from source.settings import settings
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get("/")
@@ -12,9 +23,12 @@ async def root():
 
 
 if __name__ == "__main__":
+    metadata.create_all(engine)
     uvicorn.run(
         "app:app",
         host=settings.server_host,
         port=settings.server_port,
         reload=True
     )
+
+
---
+++ source/db.py
@@ -0,0 +1,49 @@
+import sqlalchemy as sa
+import databases
+from source.settings import settings
+
+
+database = databases.Database(settings.database_url)
+metadata = sa.MetaData()
+
+user = sa.Table(
+    "user",
+    metadata,
+    sa.Column("id", sa.Integer, primary_key=True),
+    sa.Column("nickname", sa.String(50), unique=True),
+    sa.Column("status", sa.String(50)),
+    sa.Column("created_at", sa.Date),
+    sa.Column("updated_at", sa.Date),
+)
+
+recipe = sa.Table(
+    "recipe",
+    metadata,
+    sa.Column("id", sa.Integer, primary_key=True),
+    sa.Column("author", sa.Integer, sa.ForeignKey('user.id')),
+    sa.Column("created_at", sa.Date),
+    sa.Column("updated_at", sa.Date),
+    sa.Column("name", sa.String(50)),
+    sa.Column("kind", sa.String(50)),
+    sa.Column("description", sa.String),
+    sa.Column("cooking_steps", sa.String),
+    sa.Column("photo", sa.String),
+    sa.Column("likes", sa.Integer, nullable=True),
+    sa.Column("status", sa.String(50)),
+)
+
+favorite = sa.Table(
+    "favorite",
+    metadata,
+    sa.Column("user_id", sa.Integer, sa.ForeignKey('user.id')),
+    sa.Column("recipe_id", sa.Integer, sa.ForeignKey('recipe.id')),
+)
+
+hashtag = sa.Table(
+    "hashtag",
+    metadata,
+    sa.Column("recipe_id", sa.Integer, sa.ForeignKey('recipe.id')),
+    sa.Column("hashtag", sa.String(50)),
+)
+
+engine = sa.create_engine(settings.database_url)
